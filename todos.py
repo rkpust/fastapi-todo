de65ec3a106e92982d93d922515d2d7689bec51e
rkpust <rezaul.cse.pust17@gmail.com>
2025-09-04 16:38:44 +0600

    added todos for testing API

---
+++ todos.py
@@ -0,0 +1,8 @@
+todos = [
+    {'id': 1, 'name': 'Sports', 'description': 'Go to gym'},
+    {'id': 2, 'name': 'Read', 'description': 'Read 10 minute'},
+    {'id': 3, 'name': 'Shop', 'description': 'Go Shoping'},
+    {'id': 4, 'name': 'Study', 'description': 'Study for exam'},
+    {'id': 5, 'name': 'Break', 'description': 'Break for 10 minute'},
+    {'id': 6, 'name': 'Code', 'description': 'Coding for project'}
+]
