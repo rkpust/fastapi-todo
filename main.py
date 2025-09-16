db29c8d6db36ca320fa8906826c12c0435f8d830
rkpust <rezaul.cse.pust17@gmail.com>
2025-09-16 16:37:32 +0600

    added schema based [GET '/todos'] endpoint

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from todos import todos
+from typing import List
+from todos import todos, ToDo
 
 api = FastAPI()
 
@@ -7,90 +8,22 @@ api = FastAPI()
 def index():
     return {"message": "Welcome to the FastAPI based ToDo app."}
 
-@api.get('/todos')
+@api.get('/todos', response_model=List[ToDo])
 def get_todos(first_n: int = None, last_n: int = None,):
+    selected_todos = None
+
     if first_n and last_n:
         # /todos?first_n=2&last_n=4
-        return {
-            "message": f"From {first_n} to {last_n} todo list",
-            "todos": todos[first_n-1:last_n]
-            }
+        selected_todos =  todos[first_n-1:last_n]
+        return selected_todos
     elif first_n:
         # /todos?first_n=2
-        return {
-            "message": f"First {first_n} todo list",
-            "todos": todos[:first_n]
-            }
+        selected_todos =  todos[:first_n]
+        return selected_todos
     elif last_n:
         # /todos?last_n=4
-        return {
-            "message": f"Last {last_n} todo list",
-            "todos": todos[-last_n:]
-            }
+        selected_todos =  todos[-last_n:]
+        return selected_todos
     else:
         # /todos
-        return {
-            "message": "All todo list",
-            "todos": todos
-            }
-
-# /todos/1
-@api.get('/todos/{id}')
-def get_todo(id: int):
-    for todo in todos:
-        if todo['id'] == id:
-            return {
-                "message": "A todo information",
-                "todo": todo
-                }
-        
-    return {
-    "message": "No todo found",
-    }
-
-@api.post('/todos')
-def get_todos(todo: dict):
-    new_todo_id = max([todo['id'] for todo in todos]) + 1
-    new_todo = {
-        "id": new_todo_id,
-        "name": todo['name'],
-        "description": todo['description']
-    }
-
-    todos.append(new_todo)
-
-    return {
-        "message": f"A new todo is created successfully",
-        "todo": new_todo
-        }
-
-@api.put('/todos/{id}')
-def update_todo(id: int, updated_todo: dict):
-    for todo in todos:
-        if todo['id'] == id:
-            todo['name'] = updated_todo['name']
-            todo['description'] = updated_todo['description']
-
-            return {
-                "message": "A todo is updated successfully",
-                "todo": todo
-                }
-        
-    return {
-        "message": "No todo is found for updating",
-        }
-       
-@api.delete('/todos/{id}')
-def delete_todo(id: int):
-    for index, todo in enumerate(todos):
-        if todo['id'] == id:
-            deleted_todo = todos.pop(index)
-
-            return {
-                "message": "A todo is deleted successfully",
-                "todo": deleted_todo
-                }
-
-    return {
-        "message": "No todo is found for deleting",
-        }
+        return todos
